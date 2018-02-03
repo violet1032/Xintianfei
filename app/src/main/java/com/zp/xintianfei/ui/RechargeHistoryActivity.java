887783c114872e55f19d0fe0eb19e4587106447d
1053231915@qq.com <1053231915@qq.com>
2018-02-03 11:52:40 +0800

    2

---
+++ app/src/main/java/com/zp/xintianfei/ui/RechargeHistoryActivity.java
@@ -0,0 +1,32 @@
+package com.zp.xintianfei.ui;
+
+import android.view.View;
+
+import com.zp.xintianfei.R;
+import com.zp.xintianfei.ui.common.BaseActivity;
+
+public class RechargeHistoryActivity extends BaseActivity {
+
+
+
+    @Override
+    public void setRootView() {
+        super.setRootView();
+        setContentView(R.layout.activity_recharge_history);
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+    }
+
+    @Override
+    public void initWidget() {
+        super.initWidget();
+    }
+
+    @Override
+    public void widgetClick(View v) {
+        super.widgetClick(v);
+    }
+}
